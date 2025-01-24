21e0277b9f1dbb04dad71c228d2c046ef98d7abf
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2025-01-24 22:02:29 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "org",
+        loadChildren: () => import(`../app/org/org-routing.module`).then(m => m.OrgRoutingModule),
+        data: {title: 'Organisation'}
+    }
+];
